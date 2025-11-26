d94dbad88d49a9f8623fb8f743825590867658ae
Ashavmodhgill <deepmodhgill9@gmail.com>
2025-11-25 16:20:38 -0800

    updated comment model with ref part

--- src/models/comment.js
+++ src/models/comment.js
@@ -6,8 +6,20 @@ const CommentSchema = new mongoose.Schema({
         required: true,
 
     },
-    userEmail: {
+    userId: {
+       type: mongoose.Schema.Types.ObjectId,
+       ref: 'User',
+       required: true
+    },
+     onModel:{
         type: String,
+        required: true,
+        enum: ['Tweet', 'Comment']
+    },
+    commentable: {
+        type: mongoose.Schema.Types.ObjectId,
+                required: true,
+                refPath: 'onModel'
     }
 }, {timestamps: true});
 
